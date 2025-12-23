6613f1f1bd8ca2ad88845aecb9cc67cb45d25bff
AarushSrivatsa <pitlaaarushsrivatsa@gmail.com>
2025-12-23 12:39:20 +0530

    modified main.py

--- main.py
+++ main.py
@@ -3,7 +3,13 @@ from routers.user import router as user_router
 from routers.conversation import router as conversation_router
 from routers.messages import router as message_router
 
-app = FastAPI()
+app = FastAPI(
+    title="Chatbot Wrapper Backend",
+    description="""
+Built by **Aarush Srivatsa**  
+GitHub: https://github.com/AarushSrivatsa  
+Linkedin: https://www.linkedin.com/in/aarushsrivatsa/
+""")
 
 app.include_router(user_router)
 app.include_router(conversation_router)
